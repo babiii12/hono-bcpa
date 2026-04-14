35ed7ca04c260999f3e5478eac28d20c350e29b0
babiii12 <yabbabii@gmail.com>
2026-04-14 17:00:40 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,59 @@ app.get('/', (c) => {
 })
 
 export default app
+const users = []
+
+app.get('/', (c) => {
+  return c.text('API is running')
+})
+app.get('/users', (c) => {
+  return c.json(users)
+})
+app.get('/users/:id', (c) => {
+  const id = c.req.param('id')
+
+  const user = users.find(u => u.id === id)
+
+  if (!user) {
+    return c.json({ message: "User not found" }, 404)
+  }
+
+  return c.json(user)
+})
+app.post('/signup', async (c) => {
+  const body = await c.req.json()
+
+  const newUser = {
+    id: Date.now().toString(),
+    name: body.name,
+    email: body.email,
+    password: body.password
+  }
+
+  users.push(newUser)
+
+  return c.json(newUser)
+})
+app.post('/signin', async (c) => {
+  const body = await c.req.json()
+
+  const user = users.find(u => u.email === body.email)
+
+  if (!user) {
+    return c.json({ message: "User not found" }, 404)
+  }
+
+  if (user.password !== body.password) {
+    return c.json({ message: "Invalid password" }, 401)
+  }
+
+  return c.json({
+    message: "Login successful",
+    user
+  })
+})
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+})
